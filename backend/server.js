884cfcbb71821b86e68b2812949ec6039692bd3e
abdullaabdulraoof <abdullaabdulraoof66@gmail.com>
2026-08-02 21:33:31 +0530

    refactor: update import statements and logger usage in server initialization

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
 require("dotenv").config();
 
-import { listen } from "./app";
-import connectDB from "./config/db";
-import { info } from "./logger";
+const app = require("./app");
+const connectDB = require("./config/db");
+const logger = require("./logger");
+
+// Force Redis initialization
+require("./config/redis");
 
 const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-info("Server starting...");
+logger.info("Server starting...");
 
-listen(PORT, () => {
-    info(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    logger.info(`Server running on http://localhost:${PORT}`);
 });
